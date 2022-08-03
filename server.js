faaaffb10cd5f0a38391591c313890bb9e0160f4
vinayak-trivedi <90315175+vinayak-trivedi@users.noreply.github.com>
2022-08-03 17:05:56 +0530

    added old code to check if the problem is in new code

--- server.js
+++ server.js
@@ -7,36 +7,33 @@ const io = new Server(server, {
     cors: {
       origins: ['http://127.0.0.1:5500/']
     }
-});
-app.get('/', (req, res) => {
-    res.send("lift-simulation-backend")
+  });
+
+let floors = 4;
+
+app.get("", (req,res) => {
+    res.send("Lift simulation backend")
 })
 
 io.on('connection',(socket) => {
-    const userDevice =socket.handshake.query.userdevice;  
     console.log('A user connected')
-    console.log(userDevice)
-    socket.on('create', (room) => {
-        socket.join(room)
-        console.log(`Joined room ${room}`)
-    })
     socket.on('addfloor', () => {
-        io.to(userDevice).emit('addfloor', ++floors)
+        io.emit('addfloor', ++floors)
     })
     socket.on('removefloor',() => {
-        io.to(userDevice).emit('removefloor', --floors)
+        io.emit('removefloor', --floors)
     })
     socket.on('addlift', () => {
-        io.to(userDevice).emit('addlift')
+        io.emit('addlift')
     })
     socket.on('removelift', () => {
-        io.to(userDevice).emit('removelift')
+        io.emit('removelift')
     })
     socket.on('called', called)
-
 })
 
-const called = (calledOn, device) => {
-    io.to(device).emit('move', calledOn)
+const called = (calledOn) => {
+    io.emit('move', calledOn)
 }
-server.listen(process.env.PORT)
+
+server.listen(process.env.PORT || 3000)
